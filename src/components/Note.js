dd1cc13eaf3326b6606e4ad7c41911270e3e5345
wisang <wseom7@gmail.com>
2022-10-18 14:24:41 +0900

    different version

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,17 +1,12 @@
 import React from "react";
-import notes from "../notes";
 
-function createNote(note) {
+function Note(props) {
   return (
-    <div className="note">
-      <h1>{note.title}</h1>
-      <p>{note.content}</p>
-    </div>
+  <div className="note">
+    <h1>{props.title}</h1>
+    <p>{props.content}</p>
+  </div>
   );
-}
-
-function Note() {
-  return notes.map(createNote);
 }
 
 export default Note;
